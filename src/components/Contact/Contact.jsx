42cc1f11fe5e8a0011eef4cdf639d94b4b3e12ee
Yabetsu <it.jabez16@gmail.com>
2021-11-06 18:03:17 +0800

    Email Function Added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,35 @@
 import { GitHub, LinkedIn, Send } from '@mui/icons-material';
-import {
-  Box,
-  Button,
-  Grid,
-  IconButton,
-  TextField,
-  Typography
-} from '@mui/material';
+import { Box, Button, Grid, TextField, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import emailApiKey from '../../emailkey';
 
 const Contact = () => {
   const theme = useTheme();
+  const formRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        emailApiKey.SERVICE_ID,
+        emailApiKey.TEMPLATE_ID,
+        formRef.current,
+        emailApiKey.USER_ID
+      )
+      .then(
+        (result) => {
+          alert('Message sent!');
+          console.log(result.text);
+        },
+        (error) => {
+          alert('An error occurred!');
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <>
       <div
@@ -38,92 +56,92 @@ const Contact = () => {
           If you have questions just email me on the following.
         </Typography>
         <Box sx={{ textAlign: 'center' }} mt="20px">
-          <IconButton
-            aria-label="linkedInLink"
-            href="https://www.linkedin.com/in/jabez-joshua-bondoc-489016156/"
-            target="_blank"
-          >
-            <LinkedIn
-              fontSize="large"
-              color="primary"
-              sx={{ marginRight: '5px' }}
-            />
-          </IconButton>
-          <IconButton
-            aria-label="githubLink"
-            href="https://github.com/Yabetsu16"
-            target="_blank"
-          >
-            <GitHub
-              fontSize="large"
-              sx={{ color: theme.palette.mode === 'dark' ? 'white' : 'black' }}
-            />
-          </IconButton>
+          <LinkedIn
+            fontSize="large"
+            color="primary"
+            sx={{ marginRight: '5px' }}
+          />
+          <GitHub
+            fontSize="large"
+            sx={{ color: theme.palette.mode === 'dark' ? 'white' : 'black' }}
+          />
         </Box>
       </div>
-      <Grid
-        container
-        direction="row"
-        justifyContent="space-around"
-        alignItems="center"
-        spacing={5}
-        sx={{
-          backgroundColor: theme.palette.mode === 'dark' ? 'black' : 'white'
-        }}
-      >
-        <Grid item xs={10} md={5} lg={5}>
-          <TextField
-            required
-            id="firstname"
-            label="First Name"
-            fullWidth
-            variant="filled"
-          />
+      <form ref={formRef} onSubmit={handleSubmit}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-around"
+          alignItems="center"
+          spacing={5}
+          sx={{
+            backgroundColor: theme.palette.mode === 'dark' ? 'black' : 'white'
+          }}
+        >
+          <Grid item xs={10} md={5} lg={5}>
+            <TextField
+              required
+              id="firstname"
+              name="firstname"
+              label="First Name"
+              fullWidth
+              variant="filled"
+            />
+          </Grid>
+          <Grid item xs={10} md={5} lg={5}>
+            <TextField
+              required
+              id="lastname"
+              name="lastname"
+              label="Last Name"
+              fullWidth
+              variant="filled"
+            />
+          </Grid>
+          <Grid item xs={10} md={5} lg={5}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="Email"
+              fullWidth
+              variant="filled"
+            />
+          </Grid>
+          <Grid item xs={10} md={5} lg={5}>
+            <TextField
+              required
+              id="subject"
+              name="subject"
+              label="Subject"
+              fullWidth
+              variant="filled"
+            />
+          </Grid>
+          <Grid item xs={10} md={11}>
+            <TextField
+              required
+              id="message"
+              name="message"
+              label="Message"
+              multiline
+              rows={6}
+              fullWidth
+              variant="filled"
+            />
+          </Grid>
+          <Grid item md={12} lg={12} sx={{ textAlign: 'center' }} mb="40px">
+            <Button
+              type="submit"
+              variant="contained"
+              endIcon={<Send />}
+              size="large"
+            >
+              Send
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={10} md={5} lg={5}>
-          <TextField
-            required
-            id="lastname"
-            label="Last Name"
-            fullWidth
-            variant="filled"
-          />
-        </Grid>
-        <Grid item xs={10} md={5} lg={5}>
-          <TextField
-            required
-            id="email"
-            label="Email"
-            fullWidth
-            variant="filled"
-          />
-        </Grid>
-        <Grid item xs={10} md={5} lg={5}>
-          <TextField
-            required
-            id="subject"
-            label="Subject"
-            fullWidth
-            variant="filled"
-          />
-        </Grid>
-        <Grid item xs={10} md={11}>
-          <TextField
-            required
-            id="message"
-            label="Message"
-            multiline
-            rows={6}
-            fullWidth
-            variant="filled"
-          />
-        </Grid>
-        <Grid item md={12} lg={12} sx={{ textAlign: 'center' }} mb="40px">
-          <Button variant="contained" endIcon={<Send />} size="large">
-            Send
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
     </>
   );
 };
---
+++ src/emailkey.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  SERVICE_ID: `service_f5hhqhz`, // serviceID
+  USER_ID: `user_VZWQ7mRY5frWwVBdqvm3C`, //userID
+  TEMPLATE_ID: `template_itsk5c1` //templateID
+};
