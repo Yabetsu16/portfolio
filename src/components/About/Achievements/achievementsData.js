14971d06dfe39f29f0a4a23299445555a480ab39
Yabetsu <it.jabez16@gmail.com>
2022-01-14 16:24:52 +0800

    Add Udemy Certificate

--- src/components/About/Achievements/achievementsData.js
+++ src/components/About/Achievements/achievementsData.js
@@ -1,41 +1,51 @@
 import unityBadge from './unity-essentials-pathway.png';
 import ualCert from './ual-certificate.png';
 import amaDean from './ama_dean.jpg';
+import udemyCert from './udemy-certificate.jpg';
 
 export const achievements = [
   {
-    label: 'Unity',
-    title: 'Unity Essential Pathway',
-    image: unityBadge,
-    issuedBy: 'Unity Technologies',
-    issuedDate: 'JAN. 30,2021',
+    label: 'Udemy',
+    title: 'The Complete 2022 Web Development Bootcamp',
+    image: udemyCert,
+    issuedBy: 'Udemy',
+    issuedDate: 'Jan. 14, 2022',
     subheader: 'Certificate',
-    link: 'https://www.credly.com/badges/a699ed8a-4341-4bf8-be1d-c54d5c9adc92?source=linked_in_profile'
-  },
-  {
-    label: 'Futurelearn',
-    title: 'Introduction to Indie Games',
-    image: ualCert,
-    issuedBy: 'UAL Creative Computing',
-    issuedDate: 'MAR. 22,2021',
-    subheader: 'Certificate',
-    link: 'https://www.futurelearn.com/certificates/3fwxqfe'
+    link: 'https://www.udemy.com/certificate/UC-5563fbc3-fb2b-4a96-932f-10d9bbf106b1/'
   },
   {
     label: 'Sololearn',
     title: 'React + Redux course',
     image: 'https://www.sololearn.com/Certificate/1097-9659446/jpg',
     issuedBy: 'Sololearn',
-    issuedDate: 'Sept. 29,2021',
+    issuedDate: 'Sept. 29, 2021',
     subheader: 'Certificate',
     link: 'https://www.sololearn.com/Certificate/1097-9659446/jpg'
+  },
+  {
+    label: 'Futurelearn',
+    title: 'Introduction to Indie Games',
+    image: ualCert,
+    issuedBy: 'UAL Creative Computing',
+    issuedDate: 'Mar. 22, 2021',
+    subheader: 'Certificate',
+    link: 'https://www.futurelearn.com/certificates/3fwxqfe'
+  },
+  {
+    label: 'Unity',
+    title: 'Unity Essential Pathway',
+    image: unityBadge,
+    issuedBy: 'Unity Technologies',
+    issuedDate: 'Jan. 30, 2021',
+    subheader: 'Certificate',
+    link: 'https://www.credly.com/badges/a699ed8a-4341-4bf8-be1d-c54d5c9adc92?source=linked_in_profile'
   },
   {
     label: 'AMA',
     title: "Dean's lister G.W.A 1.60",
     image: amaDean,
     issuedBy: 'AMA',
-    issuedDate: 'May. 12,2017',
+    issuedDate: 'May 12, 2017',
     subheader: 'Award',
     link: amaDean
   }
